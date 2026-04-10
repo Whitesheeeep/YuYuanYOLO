bdf07f0fc8bfb09b976e9b2539a6ff59919ef40b
龚彦（Gong Yan） <128272139+Whitesheeeep@users.noreply.github.com>
2026-04-10 21:15:16 +0800

    添加按钮 3  的指令名字

--- webSocketYOLO/ButtonConfig.py
+++ webSocketYOLO/ButtonConfig.py
@@ -1,7 +1,7 @@
 BUTTON_CONFIGS = [
     {"id": 1,  "name": "重新开始"},
     {"id": 2,  "name": "重置所有YOLO接收状态"},
-    {"id": 3,  "name": "按钮3"},
+    {"id": 3,  "name": "重置ChatWindow位置"},
     {"id": 4,  "name": "按钮4"},
     {"id": 5,  "name": "按钮5"},
     {"id": 6,  "name": "强制播放提词石"},
