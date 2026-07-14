8ce5158c1e4f0a14c5f8437a2fea1aea6bf68ae1
UltralyticsAssistant <web@ultralytics.com>
2026-07-14 04:14:07 +0000

    Auto-format by https://ultralytics.com/actions

--- LangChain/LLm.py
+++ LangChain/LLm.py
@@ -2,25 +2,23 @@ import base64
 import io
 import os
 
-from PIL import Image
+import config
 from langchain_core.messages import HumanMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_openai import ChatOpenAI
-
-import config
+from PIL import Image
 
 llm = ChatOpenAI(
-    model=config.DOUBao_MODEL,
-    base_url=config.DOUBao_BASE_URL,
-    api_key=config.DOUBao_API_KEY,
-    streaming=False
+    model=config.DOUBao_MODEL, base_url=config.DOUBao_BASE_URL, api_key=config.DOUBao_API_KEY, streaming=False
 )
-prompt_template = ChatPromptTemplate.from_messages([
-    # ("system", self.base_system_content + "\n\n【实时背景资料】：\n{context}"),
-    ("system", "{context}"),
-    # MessagesPlaceholder(variable_name="input"),
-    MessagesPlaceholder(variable_name="input"), # 这里接收原始消息列表
-])
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        # ("system", self.base_system_content + "\n\n【实时背景资料】：\n{context}"),
+        ("system", "{context}"),
+        # MessagesPlaceholder(variable_name="input"),
+        MessagesPlaceholder(variable_name="input"),  # 这里接收原始消息列表
+    ]
+)
 chain = prompt_template | llm
 image_path = r"D:\Yuyuan_2\YuyuanYOLO\test2.jpg"
 if not os.path.exists(image_path):
@@ -33,12 +31,9 @@ with Image.open(image_path) as img:
     rgb_img.save(buf, format="JPEG", quality=80, optimize=True)
     rgb_img.show()
 base = base64.b64encode(buf.getvalue()).decode("utf-8")
-text =[
+text = [
     {"type": "text", "text": "请根据图片内容，15 字以内，快速回答这张图片里有什么？"},
-    {
-        "type": "image_url",
-        "image_url": {"url": f"data:image/jpeg;base64,{base}"}
-    }
+    {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{base}"}},
 ]
 print(llm.invoke([HumanMessage(content=text)]))
 print(chain.invoke({"context": "", "input": [HumanMessage(content=text)]}))
